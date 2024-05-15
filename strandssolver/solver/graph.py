c76cd70b3135e4e9dd525d1bf65bb0441a1985fb
LuNoX <maillunox@gmail.com>
2024-05-15 09:30:55 +0200

    Refactored stateful CharacterGraph to stateless CharacterGraphBuilder

--- strandssolver/solver/graph.py
+++ strandssolver/solver/graph.py
@@ -7,14 +7,12 @@ from typing import Tuple
 from strandssolver import gamestate
 
 
-class CharacterGraph:
-    def __init__(self, board: gamestate.Board) -> None:
-        self.board = board
-        self.graph = CharacterGraph.generate_graph_from_board(board)
+class CharacterGraphBuilder:
 
     @staticmethod
     def generate_graph_from_board(board: gamestate.Board) -> nx.Graph:
-        graph = CharacterGraph.diagonally_connected_grid_graph(board.shape)
+        graph = CharacterGraphBuilder.diagonally_connected_grid_graph(
+            board.shape)
 
         it = np.nditer(board.characters, flags=['multi_index'])
         node_attributes = {it.multi_index: {"character": str(character)}
@@ -43,13 +41,13 @@ def _test() -> None:
     parser = htmlparser.HTMLParser(html_reader=stubhtmlreader.StubHTMLReader())
     game = parser.parse()
     game.board.solved_states[1, 2] = True
-    graph = CharacterGraph(game.board)
+    graph = CharacterGraphBuilder.generate_graph_from_board(game.board)
 
-    pos = nx.spring_layout(graph.graph)
-    nx.draw(graph.graph, pos=pos)
-    nx.draw_networkx_labels(graph.graph,
+    pos = nx.spring_layout(graph)
+    nx.draw(graph, pos=pos)
+    nx.draw_networkx_labels(graph,
                             labels={node[0]: node[1]["character"]
-                                    for node in graph.graph.nodes(data=True)},
+                                    for node in graph.nodes(data=True)},
                             pos=pos)
     plt.show()
 
