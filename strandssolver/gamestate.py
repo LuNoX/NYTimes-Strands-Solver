3dd844bee6520bbc6db4abaead8b549eeb54c66f
LuNoX <maillunox@gmail.com>
2024-05-15 06:30:51 +0200

    Refactor

--- strandssolver/gamestate.py
+++ strandssolver/gamestate.py
@@ -10,24 +10,26 @@ class Board:
     def __init__(self,
                  characters: np.typing.NDArray[np.str_],
                  solved_states: np.typing.NDArray[np.bool_] = None,
+                 partially_solved: bool = True
                  ) -> None:
         self.characters = characters
-        self.shape = characters.shape
         if solved_states is None:
             solved_states = self._default_solved_states()
+            partially_solved = False
         self.solved_states = solved_states
+        self.partially_solved = partially_solved
 
-    def _default_solved_states(self,
-                               shape: Tuple[int, ...] = None
-                               ) -> np.typing.NDArray[np.bool_]:
-        if shape is None:
-            shape = self.characters.shape
-        return np.zeros(shape, dtype=bool)
+    @property
+    def shape(self) -> Tuple[int, ...]:
+        return self.characters.shape
+
+    def _default_solved_states(self) -> np.typing.NDArray[np.bool_]:
+        return np.zeros(self.shape, dtype=bool)
 
     @classmethod
     def characters_matrix_from_letters(cls, letters: List[str],
                                        shape: Tuple[int, ...] = DEFAULT_SHAPE
-                                       ) -> np.ndarray[np.str_]:
+                                       ) -> np.typing.NDArray[np.str_]:
         array = np.array(letters)
         characters = array.reshape(shape)
         return characters
@@ -37,6 +39,7 @@ class Board:
             "shape": self.shape,
             "characters": self.characters,
             "solved_states": self.solved_states,
+            "partial_solved": self.partially_solved
         }
         return str(representation)
 
