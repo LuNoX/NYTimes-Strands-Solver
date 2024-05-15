0de8e225c4b22b73b9a580ea51c54016bb88ab73
LuNoX <maillunox@gmail.com>
2024-05-15 14:57:32 +0200

    Completed graph implementation by adding diagonal edges

--- strandssolver/solver/graph.py
+++ strandssolver/solver/graph.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import numpy as np
 from matplotlib import pyplot as plt
+import functools
+import numpy.typing
 
 from typing import Tuple
 
@@ -29,9 +31,57 @@ class CharacterGraphBuilder:
     def diagonally_connected_grid_graph(shape: Tuple[int, ...]) -> nx.Graph:
         graph = nx.grid_graph(list(reversed(shape)))
 
-        # TODO: diagonals
+        for node in graph:
+            neigbours = CharacterGraphBuilder.valid_neighbour_indices(
+                node, shape, only_diagonals=True)
+            edges = [(node, tuple(neigbour)) for neigbour in neigbours]
+            graph.add_edges_from(edges)
 
         return graph
+
+    @staticmethod
+    @functools.cache
+    def neighbour_index_offsets(shape: Tuple[int, ...],
+                                exclude_self: bool = True,
+                                only_diagonals: bool = False,
+                                ) -> np.typing.NDArray[np.int_]:
+        # Adapted from
+        # https://stackoverflow.com/questions/34905274/how-to-find-the-neighbors-of-a-cell-in-an-ndarray
+        number_of_dims = len(shape)
+
+        # generate an (m, ndims) array containing all strings
+        # over the alphabet {0, 1, 2}:
+        offset_idx = np.indices((3,) * number_of_dims
+                                ).reshape(number_of_dims, -1).T
+
+        # use these to index into np.array([-1, 0, 1]) to get offsets
+        offsets = np.r_[-1, 0, 1].take(offset_idx)
+
+        # optional: exclude offsets of 0, 0, ..., 0 (i.e. p itself)
+        if exclude_self:
+            offsets = offsets[np.any(offsets, 1)]
+        if only_diagonals:
+            diagonal_mask = (np.sum(np.abs(offsets), axis=1) > 1)
+            offsets = offsets[diagonal_mask]
+
+        return offsets
+
+    @staticmethod
+    def valid_neighbour_indices(home_index: Tuple[int, ...],
+                                shape: Tuple[int, ...],
+                                only_diagonals: bool = False,
+                                ) -> np.typing.NDArray[np.int_]:
+        offsets = CharacterGraphBuilder.neighbour_index_offsets(
+            shape, only_diagonals=only_diagonals)
+        neighbours: np.typing.NDArray[np.int_] = home_index + offsets
+
+        in_bounds = np.all(
+            (neighbours < np.array(shape)) & (neighbours >= 0),
+            axis=1
+        )
+        neighbours = neighbours[in_bounds]
+
+        return neighbours
 
 
 def _test() -> None:
