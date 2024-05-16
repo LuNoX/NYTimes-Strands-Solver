bee4d5fff4e6f3fd4910aca923f3c34f22f35f10
LuNoX <maillunox@gmail.com>
2024-05-16 03:09:19 +0200

    Implemented StubDictionary

--- test/stubs/stubdictionary.py
+++ test/stubs/stubdictionary.py
@@ -1,8 +1,19 @@
+import os
+
 from importlib import resources
+from typing import override
 
 from strandssolver.solver import dictionary
 from test import data
 
 
 class StubDictionary(dictionary.Dictionary):
-    ...
+    DEFAULT_DICTIONARY_PATH = resources.files(data).joinpath(
+        'words_dictionary.json')
+
+    @override
+    def __init__(self, path: str | bytes | os.PathLike = None) -> None:
+        self.filter_condition = lambda word: len(word) >= 4
+        if path is None:
+            path = StubDictionary.DEFAULT_DICTIONARY_PATH
+        self.words = dictionary.DictionaryBuilder.load_keys_from_json(path)
