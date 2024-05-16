e96df0436a522354c54ac84527bfe92af4da7b07
LuNoX <maillunox@gmail.com>
2024-05-17 01:56:21 +0200

    Postponed assignment of default values in function parameters to inside the function body

--- strandssolver/models/dictionarytrie.py
+++ strandssolver/models/dictionarytrie.py
@@ -22,10 +22,17 @@ class DictionaryTrieBuilder:
     @staticmethod
     def store_trie_as_json(trie: pygtrie.Trie,
                            path: str | bytes | os.PathLike,
-                           encoding: str = DEFAULT_JSON_ENCODING,
-                           ensure_ascii: bool = DEFAULT_ENSURE_JSON_IS_ASCII,
-                           indent: int = DEFAULT_JSON_INDENTATION
+                           encoding: str = None,
+                           ensure_ascii: bool = None,
+                           indent: int = None
                            ) -> None:
+        if encoding is None:
+            encoding = DictionaryTrieBuilder.DEFAULT_JSON_ENCODING
+        if ensure_ascii is None:
+            ensure_ascii = DictionaryTrieBuilder.DEFAULT_ENSURE_JSON_IS_ASCII
+        if indent is None:
+            indent = DictionaryTrieBuilder.DEFAULT_JSON_INDENTATION
+
         with open(path, 'w', encoding=encoding) as f:
             json.dump(dict(trie), f, ensure_ascii=ensure_ascii, indent=indent)
 
