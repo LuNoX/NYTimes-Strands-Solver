f7b845e39d23138889603cbd2e0888ab759f0c02
LuNoX <maillunox@gmail.com>
2024-05-17 02:29:00 +0200

    Added timeit to time startup

--- strandssolver/solver/solver.py
+++ strandssolver/solver/solver.py
@@ -6,6 +6,11 @@ from typing import List, Tuple, Set
 
 from strandssolver.models import gamestate
 
+from strandssolver.test.stubs import stubgraph
+from strandssolver.test.stubs import stubgamestate
+from strandssolver.test.stubs import stubdictionarytrie
+from timeit import timeit
+
 
 @dataclass
 class Solver:
@@ -29,21 +34,144 @@ class Solver:
             print(f"node: {node},"" edges: {list(edges)}")
         return words
 
-    def dfs(self):
+    def dfs_edges(G, source=None, depth_limit=None, *, sort_neighbors=None):
+        """Adapted from https://networkx.org/documentation/stable/_modules/networkx/algorithms/traversal/depth_first_search.html#dfs_edges
+
+        Iterate over edges in a depth-first-search (DFS).
+
+        Perform a depth-first-search over the nodes of `G` and yield
+        the edges in order. This may not generate all edges in `G`
+        (see `~networkx.algorithms.traversal.edgedfs.edge_dfs`).
+
+        Parameters
+        ----------
+        G : NetworkX graph
+
+        source : node, optional
+           Specify starting node for depth-first search and yield edges in
+           the component reachable from source.
+
+        depth_limit : int, optional (default=len(G))
+           Specify the maximum search depth.
+
+        sort_neighbors : function (default=None)
+            A function that takes an iterator over nodes as the input, and
+            returns an iterable of the same nodes with a custom ordering.
+            For example, `sorted` will sort the nodes in increasing order.
+
+        Yields
+        ------
+        edge: 2-tuple of nodes
+           Yields edges resulting from the depth-first-search.
+
+        Examples
+        --------
+        >>> G = nx.path_graph(5)
+        >>> list(nx.dfs_edges(G, source=0))
+        [(0, 1), (1, 2), (2, 3), (3, 4)]
+        >>> list(nx.dfs_edges(G, source=0, depth_limit=2))
+        [(0, 1), (1, 2)]
+
+        Notes
+        -----
+        If a source is not specified then a source is chosen arbitrarily and
+        repeatedly until all components in the graph are searched.
+
+        The implementation of this function is adapted from David Eppstein's
+        depth-first search function in PADS [1]_, with modifications
+        to allow depth limits based on the Wikipedia article
+        "Depth-limited search" [2]_.
+
+        See Also
+        --------
+        dfs_preorder_nodes
+        dfs_postorder_nodes
+        dfs_labeled_edges
+        :func:`~networkx.algorithms.traversal.edgedfs.edge_dfs`
+        :func:`~networkx.algorithms.traversal.breadth_first_search.bfs_edges`
+
+        References
+        ----------
+        .. [1] http://www.ics.uci.edu/~eppstein/PADS
+        .. [2] https://en.wikipedia.org/wiki/Depth-limited_search
+        """
+        if source is None:
+            # edges for all components
+            nodes = G
+        else:
+            # edges for components with source
+            nodes = [source]
+        if depth_limit is None:
+            depth_limit = len(G)
+
+        get_children = (
+            G.neighbors
+            if sort_neighbors is None
+            else lambda n: iter(sort_neighbors(G.neighbors(n)))
+        )
+
         visited = set()
-        for node, data in nodes:
-            ...
+        for start in nodes:
+            if start in visited:
+                continue
+            visited.add(start)
+            stack = [(start, get_children(start))]
+            depth_now = 1
+            while stack:
+                parent, children = stack[-1]
+                for child in children:
+                    if child not in visited:
+                        yield parent, child
+                        visited.add(child)
+                        if depth_now < depth_limit:
+                            stack.append((child, get_children(child)))
+                            depth_now += 1
+                            break
+                else:
+                    stack.pop()
+                    depth_now -= 1
+
+        visited = set()
+        for start in nodes:
+            if start in visited:
+                continue
+            visited.add(start)
+            stack = [(start, get_children(start))]
+            depth_now = 1
+            while stack:
+                parent, children = stack[-1]
+                for child in children:
+                    if child not in visited:
+                        yield parent, child
+                        visited.add(child)
+                        if depth_now < depth_limit:
+                            stack.append((child, get_children(child)))
+                            depth_now += 1
+                            break
+                else:
+                    stack.pop()
+                    depth_now -= 1
 
 
 def _test() -> None:
-    from strandssolver.test.stubs import stubgraph
-    from strandssolver.test.stubs import stubgamestate
-    from strandssolver.test.stubs import stubdictionarytrie
-    solver = Solver(
-        graph=stubgraph.StubGraphBuilder.build_graph_from_board(),
-        game=stubgamestate.StubGameState(),
-        trie=stubdictionarytrie.StubDictionaryTrieBuilder.load_trie_from_json()
+    graph = stubgraph.StubGraphBuilder.build_graph_from_board()
+    graph_time = timeit(stubgraph.StubGraphBuilder.build_graph_from_board,
+                        number=1)
+    game = stubgamestate.StubGameState()
+    game_time = timeit(stubgamestate.StubGameState, number=1)
+    trie = stubdictionarytrie.StubDictionaryTrieBuilder.load_trie_from_json()
+    trie_time = timeit(
+        stubdictionarytrie.StubDictionaryTrieBuilder.load_trie_from_json,
+        number=1)
+    trie_time_from_dict = timeit(
+        stubdictionarytrie.StubDictionaryTrieBuilder.build_trie_from_dictionary,
+        number=1
     )
+    print(f"graph_time: {graph_time}, "
+          f"game_time: {game_time}, "
+          f"trie_time: {trie_time}, "
+          f"trie_time_from_dict: {trie_time_from_dict}")
+    solver = Solver(graph=graph, game=game, trie=trie)
     solver.solve()
 
 
