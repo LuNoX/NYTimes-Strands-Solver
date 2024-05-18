5f621c4198c02a0119875909ac2ef35e9c89718f
LuNoX <maillunox@gmail.com>
2024-05-18 08:44:37 +0200

    Implemented the DSFVisitor

--- strandssolver/solver/solver.py
+++ strandssolver/solver/solver.py
@@ -6,9 +6,10 @@ import itertools
 from dataclasses import dataclass
 from typing import List, Tuple, Set
 
-from strandssolver.dfs import depthfirstsearch, dfsvisitor
-from strandssolver.dfs.typing import Vertex, Node
 from strandssolver.models import gamestate
+from strandssolver.solver import strandsdfsvisitor
+from strandssolver.dfs import depthfirstsearch, dfsvisitor
+from strandssolver.dfs.typing import Node
 
 
 @dataclass
@@ -26,22 +27,20 @@ class Solver:
         with mp.Pool(mp.cpu_count()) as pool:
             args = zip(itertools.repeat(self.graph),
                        self.graph.nodes(),
-                       itertools.repeat(StrandsDFSVisitor()))
+                       itertools.repeat(strandsdfsvisitor.StrandsDFSVisitor(
+                           self.graph, self.trie))
+                       )
             pool.starmap(find_all_paths_for_node, args)
 
         return words
 
 
 def find_all_paths_for_node(graph: nx.Graph, node: Node,
-                            dfs_visitor: dfsvisitor.DFSVisitor
+                            dfs_visitor: strandsdfsvisitor.StrandsDFSVisitor
                             ) -> None:
     edges = depthfirstsearch.dfs_edges(graph, node, dfs_visitor=dfs_visitor)
     print(f"node: {node}, edges: {list(edges)}")
-
-
-class StrandsDFSVisitor(dfsvisitor.IdleDFSVisitor):
-    def finish_vertex(self, vertex: Vertex, **kwargs) -> None:
-        kwargs['dfs_completed'].remove(vertex)
+    print(dfs_visitor.words)
 
 
 def _test() -> None:
@@ -52,7 +51,7 @@ def _test() -> None:
     game = stubgamestate.StubGameState()
     trie = stubdictionarytrie.StubDictionaryTrieBuilder.load_trie_from_json()
 
-    graph = nx.grid_2d_graph(m=5, n=5)
+    # graph = nx.grid_2d_graph(m=4, n=4)
     # graph.add_edges_from([((0, 0), (1, 1)), ((0, 1), (1, 0))])
 
     solver = Solver(graph=graph, game=game, trie=trie)
---
+++ strandssolver/solver/strandsdfsvisitor.py
@@ -0,0 +1,70 @@
+import networkx as nx
+import pygtrie
+
+from typing import override, Any
+
+from strandssolver.models import graph as ssgraph
+from strandssolver.dfs import dfsvisitor, dfsexceptions
+from strandssolver.dfs.typing import Vertex, Edge
+
+
+class StrandsDFSVisitor(dfsvisitor.IdleDFSVisitor):
+    @override
+    def __init__(self, graph: nx.Graph, trie: pygtrie.Trie) -> None:
+        self.graph = graph
+        self.trie = trie
+        self.current_prefix = ""
+        self.current_path = []
+        self.words = {}
+
+    def backtrack_until_vertex(self, vertex: Vertex) -> int:
+        if len(self.current_path) <= 0:
+            raise ValueError(f'Trying to backtrack to vertex "{vertex}" '
+                             'on empty path!'
+                             'Path cannot be empty when backtracking.')
+        steps_taken = 0
+        while self.current_path[-1] != vertex:
+            self.current_path.pop()
+            steps_taken += 1
+            if len(self.current_path) <= 0:
+                raise ValueError(f'Trying to backtrack to vertex "{vertex}" '
+                                 'on path but vertex was never encountered!'
+                                 'Path must contain target vertex when '
+                                 'backtracking.')
+        return steps_taken
+
+    def get_character_from_vertex(self, vertex: Vertex,
+                                  key_for_character: Any = None) -> str:
+        if key_for_character is None:
+            key_for_character = ssgraph.CharacterGraphBuilder. \
+                DEFAULT_KEY_FOR_CHARACTER
+        return self.graph.nodes[vertex][key_for_character]
+
+    def travel_edge(self, edge: Edge) -> None:
+        origin, destination = edge
+        steps_backtracked = self.backtrack_until_vertex(origin)
+        self.current_prefix = self.current_prefix[:-steps_backtracked]
+        self.current_path.append(destination)
+        self.current_prefix += self.get_character_from_vertex(destination)
+
+    @override
+    def discover_vertex(self, vertex: Vertex, **kwargs) -> None:
+        self.current_path.append(vertex)
+        self.current_prefix += self.get_character_from_vertex(vertex).lower()
+        if not self.trie.has_subtrie(self.current_prefix):
+            raise dfsexceptions.PruneSearch
+
+    @override
+    def finish_vertex(self, vertex: Vertex, **kwargs) -> None:
+        kwargs['dfs_completed'].remove(vertex)
+        if self.trie.has_key(self.current_prefix):
+            self.words[tuple(self.current_path)] = self.current_prefix
+        self.current_path.pop()
+        self.current_prefix = self.current_prefix[:-1]
+
+    @override
+    def tree_edge(self, edge: Edge, **kwargs) -> None:
+        _, destination = edge
+        next_character = self.get_character_from_vertex(destination).lower()
+        if not self.trie.has_subtrie(self.current_prefix + next_character):
+            raise dfsexceptions.PruneSearch
