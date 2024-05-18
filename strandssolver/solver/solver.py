2755e990d891888022ab90be0670cae3a8230bf3
LuNoX <maillunox@gmail.com>
2024-05-18 05:45:20 +0200

    Implemented multiprocessing

--- strandssolver/solver/solver.py
+++ strandssolver/solver/solver.py
@@ -1,10 +1,13 @@
 import networkx as nx
 import pygtrie
+import multiprocessing as mp
+import itertools
 
 from dataclasses import dataclass
 from typing import List, Tuple, Set
 
-from strandssolver.dfs import depthfirstsearch
+from strandssolver.dfs import depthfirstsearch, dfsvisitor
+from strandssolver.dfs.typing import Vertex, Node
 from strandssolver.models import gamestate
 
 
@@ -19,17 +22,26 @@ class Solver:
 
     def find_all_words(self) -> List[Tuple[str, List[Tuple[int, ...]]]]:
         words = []
-        # TODO: parallelize this
-        for node in self.graph.nodes():
-            # DFS
-            # Break if prefix not in dict
-            # append words and paths to list
 
-            for edge in depthfirstsearch.dfs_edges(self.graph, depth_limit=6):
-                continue
-            print(f"node: {node},"" edges: {list(edges)}")
-            break
+        with mp.Pool(mp.cpu_count()) as pool:
+            args = zip(itertools.repeat(self.graph),
+                       self.graph.nodes(),
+                       itertools.repeat(StrandsDFSVisitor()))
+            pool.starmap(find_all_paths_for_node, args)
+
         return words
+
+
+def find_all_paths_for_node(graph: nx.Graph, node: Node,
+                            dfs_visitor: dfsvisitor.DFSVisitor
+                            ) -> None:
+    edges = depthfirstsearch.dfs_edges(graph, node, dfs_visitor=dfs_visitor)
+    print(f"node: {node}, edges: {list(edges)}")
+
+
+class StrandsDFSVisitor(dfsvisitor.IdleDFSVisitor):
+    def finish_vertex(self, vertex: Vertex, **kwargs) -> None:
+        kwargs['dfs_completed'].remove(vertex)
 
 
 def _test() -> None:
@@ -39,6 +51,10 @@ def _test() -> None:
     graph = stubgraph.StubGraphBuilder.build_graph_from_board()
     game = stubgamestate.StubGameState()
     trie = stubdictionarytrie.StubDictionaryTrieBuilder.load_trie_from_json()
+
+    graph = nx.grid_2d_graph(m=5, n=5)
+    # graph.add_edges_from([((0, 0), (1, 1)), ((0, 1), (1, 0))])
+
     solver = Solver(graph=graph, game=game, trie=trie)
     solver.solve()
 
